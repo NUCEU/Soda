326cf228bf42acb33c3d888cfa43305a120ce673
youngju <97087966+NUCEU@users.noreply.github.com>
2025-12-01 23:01:26 +0900

    first push
    first

---
+++ soda/client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className='site-footer'>
+      <p>YOUNGJU'S CURRENT INTERESTS</p>
+      <div className='container'>
+        <small> 2025 Soda Portfolio </small>
+        <small>github.com/NUCEU/Soda</small>
+      </div>
+    </footer>
+  );
+}
